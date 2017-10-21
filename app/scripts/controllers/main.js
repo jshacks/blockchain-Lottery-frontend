9ca48593d219da44f36656aab5e28606692b5a88
Raziel <matei.petroschi@gmail.com>
2017-10-21 12:39:08 +0300

    second commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('frontApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
   });
