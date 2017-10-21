8da8357bf0f59615d46c2b000ec1931801bc8363
Raziel <matei.petroschi@gmail.com>
2017-10-21 16:00:46 +0300

    Stats and Post

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,8 +7,56 @@
  * # MainCtrl
  * Controller of the frontApp
  */
+
 angular.module('frontApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.x = 5;
     $scope.y = $scope.x + 7;
+    $scope.lots = [{name: 'aurel', status: 'off', partHref: "https:\\www.google.com"},{name: "numarul 2", status: '78'}]
+
+    //192.168.1.58/api/signup
+    //$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+
+    $http({
+      method: 'GET',
+      url: '/someUrl'
+    }).then(function successCallback(response) {
+      $scope.lots = response.data;
+      console.log("am primit asta");
+      console.log(response);
+    }, function errorCallback(response) {
+      console.log(response);
+    });
+
+    $scope.post = function(){
+      console.log('click');
+        //var data = {email: "email@service.com", password: "melciScoiciRaciCraci"};
+
+        /*
+        var data = $.param({
+              email: "email@service.com",
+              password: "melciScoiciRaciCraci"
+        });*/
+        /*
+        var config = {
+            headers : {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }*/
+
+        var req = {
+            method: 'POST',
+            url: 'http://192.168.1.58:8080/api/signup',
+            data: {email: "email3@service.com", password: "melciScoiciRaciCraci"}
+            }
+        $http(req).then(function(response){console.log(response); console.log('Blanao');}, function(response){console.log(response); console.log('NO POST')});
+
+        /*
+        $http.post('192.168.1.58:8080/api/signup', data, config)
+              .then(function(){console.log("success")},function(){console.log('FAIL');})*/
+
+   }
+
+
+
   });
